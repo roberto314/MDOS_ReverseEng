e76cc118e44d48ac8a766a809b16fd7fc98ddaf7
roberto314 <100proentoffner@gmail.com>
2026-02-13 08:32:17 +0100

    removed DiskImageMainpulator

--- python/DiskImageManipulator/batchextract.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-prog="./ditool.py fdos extract "
-
-for f in `find . -type f -name '*.DSK' -o -name '*.dsk'`
-do
-    echo "---------------- converting:                 "$f
-    $prog ${f} " nowait"
-done
